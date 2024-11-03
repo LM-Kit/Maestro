e1c4a1201c7a49c4e51624dfdf7606195beff047
BeepBeepBopBop <evan.carrere@gmail.com>
2024-11-03 13:18:36 +0100

    ui in progress

--- LM-Kit-Maestro/ViewModels/Pages/AssistantsPageViewModel.cs
+++ LM-Kit-Maestro/ViewModels/Pages/AssistantsPageViewModel.cs
@@ -1,20 +1,36 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using LMKitMaestro.Data;
-using Microsoft.Extensions.Logging;
-using LMKitMaestro.Helpers;
+﻿using CommunityToolkit.Mvvm.Input;
 using LMKitMaestro.Services;
 using Mopups.Interfaces;
 
 namespace LMKitMaestro.ViewModels
 {
-    public sealed class AssistantsPageViewModel : PageViewModelBase
+    public partial class AssistantsPageViewModel : PageViewModelBase
     {
+        private string _input = string.Empty;
+        public string Input
+        {
+            get => _input;
+            set
+            {
+                _input = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ModelListViewModel ModelListViewModel { get; }
 
-        public AssistantsPageViewModel(INavigationService navigationService, IPopupService popupService, IPopupNavigation popupNavigation, ModelListViewModel modelListViewModel) : base(navigationService, popupService, popupNavigation)
+        public LMKitService LMKitService { get; }
+
+        public AssistantsPageViewModel(INavigationService navigationService, IPopupService popupService, IPopupNavigation popupNavigation, ModelListViewModel modelListViewModel, LMKitService lMKitService) : base(navigationService, popupService, popupNavigation)
         {
             ModelListViewModel = modelListViewModel;
+            LMKitService = lMKitService;
+        }
+
+        [RelayCommand]
+        public void RunAssistant()
+        {
+
         }
     }
 }
