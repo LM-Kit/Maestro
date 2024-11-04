acca58827c94d8a1863d273d9500060b55b89b70
BeepBeepBopBop <evan.carrere@gmail.com>
2024-11-04 18:35:45 +0100

    change scrollbar color

--- LM-Kit-Maestro/ViewModels/Pages/AssistantsPageViewModel.cs
+++ LM-Kit-Maestro/ViewModels/Pages/AssistantsPageViewModel.cs
@@ -1,14 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using LMKitMaestro.Data;
-using Microsoft.Extensions.Logging;
-using LMKitMaestro.Helpers;
-using LMKitMaestro.Services;
+﻿using LMKitMaestro.Services;
 using Mopups.Interfaces;
 
 namespace LMKitMaestro.ViewModels
 {
-    internal class AssistantsPageViewModel : PageViewModelBase
+    internal partial class AssistantsPageViewModel : PageViewModelBase
     {
         public AssistantsPageViewModel(INavigationService navigationService, IPopupService popupService, IPopupNavigation popupNavigation) : base(navigationService, popupService, popupNavigation)
         {
