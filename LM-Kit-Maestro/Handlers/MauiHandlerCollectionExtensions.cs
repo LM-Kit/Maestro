5348a24591e890e986b99f3d1e206db9bcaa81fc
BeepBeepBopBop <evan.carrere@gmail.com>
2024-11-22 11:51:26 +0100

    adding CustomEditor control, removing plainer package reference

--- LM-Kit-Maestro/Handlers/MauiHandlerCollectionExtensions.cs
+++ LM-Kit-Maestro/Handlers/MauiHandlerCollectionExtensions.cs
@@ -7,6 +7,7 @@ public static class MauiHandlerCollectionExtensions
     public static IMauiHandlersCollection AddCustomHandlers(this IMauiHandlersCollection handlers)
     {
         handlers.AddHandler(typeof(CustomEntry), typeof(CustomEntryHandler));
+        handlers.AddHandler(typeof(CustomEditor), typeof(CustomEditorHandler));
         handlers.AddHandler(typeof(StatefulContentView), typeof(StatefulContentViewHandler));
 
         return handlers;
