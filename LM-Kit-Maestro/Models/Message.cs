a223b147a727492770ec8a3a25c2df9a927372f3
Loïc Carrère <loic.carrere@gmail.com>
2024-11-23 15:27:03 +0100

    Removed legacy Chat Playground mentions (#24)

--- LM-Kit-Maestro/Models/Message.cs
+++ LM-Kit-Maestro/Models/Message.cs
@@ -1,16 +1,8 @@
-﻿
-namespace LMKit.Maestro.Models;
+﻿namespace LMKit.Maestro.Models;
 
 public sealed class Message
 {
     public string? Text { get; set; }
 
     public MessageSender Sender { get; set; }
-}
-
-public enum MessageSender
-{
-    Undefined,
-    User,
-    Assistant
 }
