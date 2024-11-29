161e1c189dae1a929a06799caca35dec4d84ce66
Loïc Carrère <loic.carrere@gmail.com>
2024-11-29 09:52:30 +0100

    Updated unit tests and CI configuration

--- tests/Services/DummyLLmFileManager.cs
+++ tests/Services/DummyLLmFileManager.cs
@@ -10,7 +10,17 @@ internal class DummyLLmFileManager : ILLMFileManager
     public ObservableCollection<Uri> UnsortedModels { get; } = new ObservableCollection<Uri>();
 
     public bool FileCollectingInProgress { get; private set; }
-    public string ModelsFolderPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string ModelsFolderPath
+    {
+        get
+        {
+            return "";
+        }
+        set
+        {
+
+        }
+    }
 
 #pragma warning disable 67
     public event EventHandler? FileCollectingCompleted;
