9e10688b2244d45537bef7de0080ff53d4a04ad4
Loïc Carrère <loic.carrere@gmail.com>
2024-12-07 17:52:09 +0100

    Introduced chat tooltips

---
+++ LM-Kit-Maestro/UI/TooltipLabels.cs
@@ -0,0 +1,10 @@
+﻿namespace LMKit.Maestro.UI
+{
+    internal static class TooltipLabels
+    {
+        public const string REGENERATE_RESPONSE = "Regenerate response";
+        public const string PREVIOUS_GENERATED_RESPONSE = "Previous generated response";
+        public const string NEXT_GENERATED_RESPONSE = "Next generated response";
+        public const string COPY_MESSAGE = "Copy message";
+    }
+}
