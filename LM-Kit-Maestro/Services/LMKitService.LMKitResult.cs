59d1efca74754781bacaedabba962932b79e9162
Loïc Carrère <loic.carrere@gmail.com>
2024-12-11 18:43:56 +0100

    Introduced logic to propagate context state (#65)

---
+++ LM-Kit-Maestro/Services/LMKitService.LMKitResult.cs
@@ -0,0 +1,13 @@
+﻿namespace LMKit.Maestro.Services;
+
+public partial class LMKitService
+{
+    public sealed class LMKitResult
+    {
+        public Exception? Exception { get; set; }
+
+        public LMKitTextGenerationStatus Status { get; set; }
+
+        public object? Result { get; set; }
+    }
+}
---
+++ LM-Kit-Maestro/Services/LMKitService.NotifyModelStateChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace LMKit.Maestro.Services;
+
+public partial class LMKitService
+{
+    public class NotifyModelStateChangedEventArgs : EventArgs
+    {
+        public Uri FileUri { get; }
+
+        public NotifyModelStateChangedEventArgs(Uri fileUri)
+        {
+            FileUri = fileUri;
+        }
+    }
+}
