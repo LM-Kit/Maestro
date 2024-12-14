b15e671f4f35f1d83969e617919751fa5dd09a3f
BeepBeepBopBop <116374330+BeepBeepBopBop@users.noreply.github.com>
2024-12-14 14:19:04 +0100

    Update UIConstants.cs

--- LM-Kit-Maestro/UI/UIConstants.cs
+++ LM-Kit-Maestro/UI/UIConstants.cs
@@ -20,22 +20,37 @@ public static class UIConstants
 
     public const double AlertPopupWidth = 400;
 
+    public const double ModelSelectionButtonMaxWidth = 568;
+
+    public const double ChatPageSidebarWidth = 300;
+
+#if WINDOWS || NET9_0
     public const double TabBarHeight = 48;
 
     public const double PageTopBarHeight = 64;
 
-    public const double ModelSelectionButtonMaxWidth = 568;
-
     public const double MinimizedHeaderButtonWidth = ChatWindowLayoutMinimumWidth - (16 * 2);
 
     public const double ModelSelectionButtonHeight = 48;
-
-    public const double ChatPageSidebarWidth = 300;
 
     public const double HeaderHorizontalMargin = 12;
 
     public const double ChatPageSideTogglesWidth = (ChatPageToggleButtonWidth * 2) + 16 + 8;
 
     public const double ChatPageToggleButtonWidth = 32;
+#elif MACCATALYST
+    public const double TabBarHeight = 48;
 
+    public const double PageTopBarHeight = 64;
+
+    public const double MinimizedHeaderButtonWidth = ChatWindowLayoutMinimumWidth - (16 * 2);
+
+    public const double ModelSelectionButtonHeight = 48;
+
+    public const double HeaderHorizontalMargin = 12;
+
+    public const double ChatPageSideTogglesWidth = (ChatPageToggleButtonWidth * 2) + 16 + 8;
+
+    public const double ChatPageToggleButtonWidth = 32;
+#endif
 }
