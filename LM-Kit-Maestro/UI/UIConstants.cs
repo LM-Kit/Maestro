9d342c453d3c4725750f57d68a28048a61ff9306
BeepBeepBopBop <evan.carrere@gmail.com>
2024-12-14 15:55:56 +0100

    adding platform specific css

--- LM-Kit-Maestro/UI/UIConstants.cs
+++ LM-Kit-Maestro/UI/UIConstants.cs
@@ -8,7 +8,6 @@ namespace LMKit.Maestro.UI;
 
 public static class UIConstants
 {
-    public const double ChatMessageMaximumWidth = 800;
 
     public const double ChatWindowLayoutMinimumWidth = 720;
 
@@ -18,23 +17,28 @@ public static class UIConstants
     
     public const double AlertPopupWidth = 400;
 
-    public const double ModelSelectionButtonMaxWidth = 568;
 
     public const double ChatPageSidebarWidth = 300;
 
 #if MACCATALYST
-    public const double TabBarHeight = 64;
+    public const double ChatMessageMaximumWidth = 1000;
 
-    public const double PageTopBarHeight = 92;
+    public const double TabBarHeight = 60;
+
+    public const double PageTopBarHeight = 80;
     
-    public const double ModelSelectionButtonHeight = 64;
+    public const double ModelSelectionButtonHeight = 60;
 
     public const double HeaderHorizontalMargin = 20;
 
     public const double ChatPageSideTogglesWidth = (ChatPageToggleButtonWidth * 2) + 16 + 8;
 
-    public const double ChatPageToggleButtonWidth = 48;
+    public const double ChatPageToggleButtonWidth = 40;
+    
+    public const double ModelSelectionButtonMaxWidth = 710;
 #else
+    public const double ChatMessageMaximumWidth = 800;
+
     public const double TabBarHeight = 48;
 
     public const double PageTopBarHeight = 64;
@@ -46,5 +50,7 @@ public static class UIConstants
     public const double ChatPageSideTogglesWidth = (ChatPageToggleButtonWidth * 2) + 16 + 8;
 
     public const double ChatPageToggleButtonWidth = 32;
+
+    public const double ModelSelectionButtonMaxWidth = 568;
 #endif
 }
