0246b2c9e8a7985a73e42e05097612ad64ccd22e
BeepBeepBopBop <116374330+BeepBeepBopBop@users.noreply.github.com>
2024-12-14 15:06:15 +0100

    Update lmkitmaestro.js

--- LM-Kit-Maestro/wwwroot/js/lmkitmaestro.js
+++ LM-Kit-Maestro/wwwroot/js/lmkitmaestro.js
@@ -1,2 +1,17 @@
 // This is a JavaScript module that is loaded on demand. It can export any number of
 // functions, and may import other JavaScript modules if required.
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const platform = window.navigator.userAgent;
+        let platformClass = "";
+
+        if (platform.includes("Windows")) {
+            platformClass = "windows";
+        } else if (platform.includes("Macintosh")) {
+            platformClass = "mac";
+        }
+
+        if (platformClass) {
+            document.body.classList.add(platformClass);
+        }
+    });
