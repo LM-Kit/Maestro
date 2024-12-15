aa135619a411e1972073a8a3b70909aade62a447
Loïc Carrère <loic.carrere@gmail.com>
2024-12-15 10:05:02 +0100

    version update

--- LM-Kit-Maestro/AppConstants.cs
+++ LM-Kit-Maestro/AppConstants.cs
@@ -2,7 +2,7 @@
 
 internal static class AppConstants
 {
-    public const string AppVersion = "0.1.0";
+    public const string AppVersion = "0.1.1";
     public const string AppName = "LM-Kit Maestro";
     public static string AppNameWithVersion => $"{AppName} {AppVersion}";
 
