fee176bb26fdae3fb506260bc165f14f70d40529
Loïc Carrère <loic.carrere@gmail.com>
2024-12-19 15:38:34 +0100

    code files refactoring

---
+++ LM-Kit-Maestro/Services/LLMFileManager.ModelDownloadingProgressedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿namespace LMKit.Maestro.Services;
+
+public partial class LLMFileManager
+{
+    public class ModelDownloadingProgressedEventArgs : EventArgs
+    {
+        public string ModelFilePath { get; }
+
+        public long BytesRead { get; }
+
+        public long? ContentLength { get; }
+
+        public double Progress { get; }
+
+        public ModelDownloadingProgressedEventArgs(string modelFilePath, long bytesRead, long? contentLength, double progress)
+        {
+            ModelFilePath = modelFilePath;
+            BytesRead = bytesRead;
+            ContentLength = contentLength;
+            Progress = progress;
+        }
+    }
+}
