58c3124f744596af7dcb2be2c623b4a7c6736b75
Loïc Carrère <loic.carrere@gmail.com>
2024-12-21 15:42:35 +0100

    binding improvements

--- LM-Kit-Maestro/ViewModels/ModelListViewModel.cs
+++ LM-Kit-Maestro/ViewModels/ModelListViewModel.cs
@@ -162,6 +162,8 @@ namespace LMKit.Maestro.ViewModels
                     index++;
                 }
             }
+
+            OnPropertyChanged(nameof(DownloadedCount));
         }
 
         private void AddNewModel(ModelCard modelCard)
