d9cef4955485b293c6f02a6a2ed03a94fc727674
BeepBeepBopBop <116374330+BeepBeepBopBop@users.noreply.github.com>
2025-01-23 19:02:37 +0100

    progress

---
+++ LM-Kit-Maestro/UI/Razor/Components/NumericSetting.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using System.Numerics;
+
+namespace LMKit.Maestro.UI.Razor.Components;
+
+public partial class NumericSetting<T> : ComponentBase where T : struct, INumber<T>
+{
+    private string _inputText = "";
+
+    [Parameter] public required string Title { get; set; }
+    [Parameter] public required T Value { get; set; }
+    [Parameter] public required T MinValue { get; set; }
+    [Parameter] public required T MaxValue { get; set; }
+
+    private void OnInputFocusOut()
+    {
+        ValidateSettingValue();
+    }
+
+    private void OnKeyDown(KeyboardEventArgs keyboardEventArgs)
+    {
+        if (keyboardEventArgs.Key == "Enter")
+        {
+            ValidateSettingValue();
+        }
+    }
+
+    private void ValidateSettingValue()
+    {
+        //if (int.TryParse(_inputText, out int parsedValue))
+        //{
+        //    Value = parsedValue;
+        //}
+        //else
+        //{
+        //    _inputText = Value.ToString()!;
+        //}
+    }
+}
