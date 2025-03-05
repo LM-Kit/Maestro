7709d5707e3c8dc74ac7d643f30a2cf250893991
BeepBeepBopBop <116374330+BeepBeepBopBop@users.noreply.github.com>
2025-03-05 11:24:48 +0100

    fix observableproperty induced warning, reorganize code

--- LM-Kit-Maestro/ViewModels/Pages/ChatPageViewModel.cs
+++ LM-Kit-Maestro/ViewModels/Pages/ChatPageViewModel.cs
@@ -1,42 +1,29 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using LMKit.Maestro.Data;
 using LMKit.Maestro.Services;
-using Microsoft.Extensions.Logging;
 namespace LMKit.Maestro.ViewModels;
 
 public partial class ChatPageViewModel : ViewModelBase
 {
-    private readonly ILogger<ChatPageViewModel> _logger;
-    private readonly IMaestroDatabase _database;
-    private readonly ILLMFileManager _llmFileManager;
+    public SettingsViewModel SettingsViewModel { get; }
 
     [ObservableProperty]
-    private bool _chatsSidebarIsToggled;
+    public partial bool ChatsSidebarIsToggled { get; set; }
 
     [ObservableProperty]
-    private bool _settingsSidebarIsToggled;
-
-    [ObservableProperty]
-    private SettingsViewModel _settingsViewModel;
+    public partial bool SettingsSidebarIsToggled { get; set; }
 
     public LMKitService LmKitService { get; }
     public ConversationListViewModel ConversationListViewModel { get; }
     public ModelListViewModel ModelListViewModel { get; }
 
     public ChatPageViewModel(ConversationListViewModel conversationListViewModel, ModelListViewModel modelListViewModel,
-        ILogger<ChatPageViewModel> logger, IMaestroDatabase database,
-        LMKitService lmKitService, ILLMFileManager llmFileManager, SettingsViewModel settingsViewModel)
+        LMKitService lmKitService, SettingsViewModel settingsViewModel)
     {
-        _logger = logger;
         ConversationListViewModel = conversationListViewModel;
         ModelListViewModel = modelListViewModel;
-        _database = database;
-        _llmFileManager = llmFileManager;
         LmKitService = lmKitService;
         SettingsViewModel = settingsViewModel;
-
-        ConversationListViewModel.Conversations.CollectionChanged += OnConversationListChanged;
     }
 
     [RelayCommand]
@@ -61,21 +48,5 @@ public partial class ChatPageViewModel : ViewModelBase
     public async Task DeleteConversation(ConversationViewModel conversationViewModel)
     {
         await ConversationListViewModel.DeleteConversation(conversationViewModel);
-
-        if (ConversationListViewModel.Conversations.Count == 0)
-        {
-            StartNewConversation();
-        }
-        else if (conversationViewModel == ConversationListViewModel.CurrentConversation)
-        {
-            ConversationListViewModel.CurrentConversation = ConversationListViewModel.Conversations.First();
-        }
-    }
-    private void OnConversationListChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-    {
-        if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add && ConversationListViewModel.Conversations.Count == 1)
-        {
-            ConversationListViewModel.CurrentConversation = ConversationListViewModel.Conversations[0];
-        }
     }
 }
