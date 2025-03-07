a45660661b8633a6c9489b2cc490c158a2ed354f
BeepBeepBopBop <116374330+BeepBeepBopBop@users.noreply.github.com>
2025-03-07 17:52:11 +0100

    cc

--- LM-Kit-Maestro/UI/UIConstants.cs
+++ LM-Kit-Maestro/UI/UIConstants.cs
@@ -5,37 +5,11 @@ namespace LMKit.Maestro.UI;
 public static class UIConstants
 {
     public const int ChatWindowLayoutMinimumWidth = 720;
-
     public const double WindowMinimumWidth = 568;
-
     public const double WindowMinimumHeight = 600;
-
-    public const double AlertPopupWidth = 400;
-
-    public const double ChatPageSidebarWidth = 300;
-
-    public const double ChatMessageMaximumWidth = 800;
-
-    public const double TabBarHeight = 48;
-
-    public const double PageTopBarHeight = 64;
-
-    public const double ModelSelectionButtonHeight = 48;
-
-    public const double HeaderHorizontalMargin = 12;
-
-    public const double ChatPageSideTogglesWidth = (ChatPageToggleButtonWidth * 2) + 16 + 8;
-
-    public const double ChatPageToggleButtonWidth = 32;
-
-    public const double ModelSelectionButtonMaxWidth = 568;
-
-    public const int ChatSidebarInitialWidth = 300;
-
-    public const int ChatSidebarMinimumWidth = 180;
-
+    public const int ChatPageSidebarWidth = 300;
+    public const int ChatSidebarMinimumWidth = 178;
     public const float ChatSidebarMaxWidthPercents = 0.3f;
-
     public const float ChatSidebarInitialRatio = 0.2f;
 
     public static class Colors
